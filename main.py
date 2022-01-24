722ad6dd92d8eaa1792896318076df746f0b1aa4
Bruhg <92408170+skyline-seven@users.noreply.github.com>
2022-01-24 18:55:12 +0000

    added say command renamed way commands are said

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ i = 1
 def IDE():
   ide = input("Type command:")
 
-  if ide == "test":
+  if ide == "/test":
     print("test complete")
+  elif ide == "/help":
+    print("/test: test command \n/help: get a list of commands \n/say: type in a set of words and the machine will repeat them")
+  elif ide == "/say":
+  
+    prnt=input("")
+    print(prnt)
   else :
-    print("error")
+    print("Error type help for a list of commands")
 
     
 while i == 1:
